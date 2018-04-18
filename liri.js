e665725caa253e50bdc137fa18d6b20f93b2be82
Abraham <abeis@msn.com>
2018-04-17 21:37:47 -0500

    added twitter posting and geting last tweet

--- liri.js
+++ liri.js
@@ -1,6 +1,26 @@
 require("dotenv").config();
 
 var keys = require('./keys');
+var Twitter = require('twitter');
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+ var client = new Twitter(keys.twitter);
+
+ var params = {screen_name: 'DabeNeko'};
+
+
+ client.get('statuses/user_timeline', params, function(error, tweets, response) {
+   if (!error) {
+     console.log(tweets[0].text);
+   }
+ });
+
+
+ /**
+  * Post Message to twitter
+  */
+ client.post('statuses/update', {status: 'BATMAN'},  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body. 
+    console.log(response);  // Raw response object. 
+  });
