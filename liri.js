7ad4b163934955832cbd332cfb43df07102ac882
Abraham Anderson <abeis@msn.com>
2018-04-20 16:52:29 -0500

    got movie api working

--- liri.js
+++ liri.js
@@ -6,14 +6,18 @@ let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
 let params = {screen_name: 'DabeNeko'};
 let command = process.argv[2]
+let commandValue = process.argv[3]
 
 switch(command){
   case 'my-tweets':
-      getTweets();
+      getTweets(commandValue);
       break;
   case 'spotify-this-song':
-      getSong()
+      getSong(commandValue)
       break;
+  case 'movie-this':
+      getMovie(commandValue)
+      break;    
   default:
       console.log('Please Input a command eg node liri.js my-tweets')    
 }
@@ -52,6 +56,21 @@ function getSong(title = 'All the Small Things'){
     .catch(function(err) {
       console.log(err);
     });
+}
+
+function getMovie(movie = 'Mr. Nobody'){
+  movie = movie.replace(' ' , '+');
+  var request = require('request');
+request('http://www.omdbapi.com/?apikey=trilogy&t=' + movie , function (error, response, body) {
+  if(error){
+    console.log('error:', error); // Print the error if one occurred
+  }else{
+    console.log(body)
+    const movieData = JSON.parse(body);
+    console.log(`Title:${movieData.Title} \nYear:${movieData.Year} \nIMBD Rateing: ${movieData.imdbRating} \nTomatoes Rateing:movieData.Ratings[1].Value} \nCounty:${movieData.Country} \nPlot:${movieData.Plot} \nActors: ${movieData.Actors}`); // Print the HTML for the Google homepage.
+  }
+});
+ 
 }
 
 
